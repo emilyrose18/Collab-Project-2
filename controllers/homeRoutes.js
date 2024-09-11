52ca3233780343d13966fdcd2af38749e3b75aea
Efrain Del Campo <efraindelcampo@Efrains-MacBook-Pro.local>
2024-09-11 16:37:55 -0700

    fixed folder layout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,11 +19,11 @@
  });
 
 //  router.get('/login', (req, res) => {
-//    // If a session exists, redirect the request to the homepage
-//    if (req.session.logged_in) {
-//      res.redirect('/');
-//      return;
-//    }
+// //    // If a session exists, redirect the request to the homepage
+// //    if (req.session.logged_in) {
+// //      res.redirect('/');
+// //      return;
+// //    }
 
 //    res.render('login');
 //  });
